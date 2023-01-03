b612becf00c3988a0d3346ed49356aedb1ad933c
Ben Doyle <benjaminmarkdoyle@gmail.com>
2023-01-03 16:36:14 -0700

    one more

--- pages/postauth/EmptyTableBody.js
+++ pages/postauth/EmptyTableBody.js
@@ -1,11 +1,8 @@
 export default function EmptyTableBody(props) {
-    return (
+    return props.message ? (
         <tr>
             <td colSpan="4">
                 <div className="relative mt-4 mb-2">
-                    {/* <div className="absolute inset-0 flex items-center" aria-hidden="true">
-                        <div className="w-full border-t border-gray-300" />
-                    </div> */}
                     <div className="relative flex justify-center">
                         <span className="bg-white px-2 text-sm text-gray-500 flex">
                             {props.message}
@@ -15,5 +12,5 @@ export default function EmptyTableBody(props) {
                 </div>
             </td>
         </tr>
-    )
+    ) : null
 }

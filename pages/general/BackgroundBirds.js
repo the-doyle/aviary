20f96a148582121e64020754a1d7c45bc50975d2
Benjamin Doyle <bendoyle@Benjamins-MacBook-Pro.local>
2023-01-18 13:40:19 -0700

    fixed alt image error

--- pages/general/BackgroundBirds.js
+++ pages/general/BackgroundBirds.js
@@ -7,11 +7,11 @@ export default function BackgroundBirds() {
     return (
         <div className='max-h-screen'>
             <div className='absolute invisible md:visible opacity-20 mt-5 z-0 grid grid-flow-row grid-rows-12 grid-cols-12'>
-                {Array.from({ length: 100 }, (_, i) => i % 5 == 0 ? <Image alt="pic"  key={i} alt='bird' src={i % 15 == 0 ? finch : i % 10 == 0 ? woodpecker : bird} className="bg-transparent" /> : <p key={i}></p> )}
+                {Array.from({ length: 100 }, (_, i) => i % 5 == 0 ? <Image alt="pic"  key={i} src={i % 15 == 0 ? finch : i % 10 == 0 ? woodpecker : bird} className="bg-transparent" /> : <p key={i}></p> )}
             </div>
 
             <div className='absolute visible md:invisible opacity-20 mt-5 z-0 grid grid-flow-row grid-rows-12 grid-cols-12'>
-                {Array.from({ length: 350 }, (_, i) => i % 17 == 0 ? <Image alt="pic"  key={i} alt='bird' src={i % 51 == 0 ? finch : i % 34 == 0 ? woodpecker : bird} className="bg-transparent" /> : <p key={i}></p> )}
+                {Array.from({ length: 350 }, (_, i) => i % 17 == 0 ? <Image alt="pic"  key={i} src={i % 51 == 0 ? finch : i % 34 == 0 ? woodpecker : bird} className="bg-transparent" /> : <p key={i}></p> )}
             </div>
         </div>
     )

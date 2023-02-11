977240f0633b048223bfbbb99fea5d6cf0178432
Benjamin Doyle <bendoyle@Benjamins-MacBook-Pro.local>
2023-02-10 20:06:48 -0700

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import '../styles/globals.css'
-import nProgress from 'nprogress'
 import Head from 'next/head'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
 	const [supabase] = useState(() => createBrowserSupabaseClient())
@@ -20,6 +20,7 @@ export default function App({ Component, pageProps }) {
 			<SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
 				<main>
 					<Component {...pageProps} />
+					<Analytics />
 				</main>
 			</SessionContextProvider>
 		</>

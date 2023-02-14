494ca50059ca543b3edddd02107dd3ea58cf18c2
Ben Doyle <benjaminmarkdoyle@gmail.com>
2023-02-14 15:31:53 -0700

    removed a tag, added Link

--- pages/postauth/PostAuthNav.js
+++ pages/postauth/PostAuthNav.js
@@ -14,7 +14,6 @@ const pages = [
     { name: 'Goals', href: '/goals' },
     { name: 'Progress', href: '/progress' },
     { name: 'Aviary', href: '/aviary' },
-    // { name: 'Profile', href: '/profile' },
 ]
 
 export default function PostAuthNav(props) {
@@ -91,12 +90,12 @@ export default function PostAuthNav(props) {
                                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                         <Menu.Item>
                                             {({ active }) => (
-                                            <a
+                                            <Link
                                                 href="/profile"
                                                 className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                                             >
                                                 Settings
-                                            </a>
+                                            </Link>
                                             )}
                                         </Menu.Item>
                                         <Menu.Item>
